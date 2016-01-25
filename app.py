17a649a941ca7845bfdac563e4cf65d95d4cf3d2
d87 <d87d87@gmail.com>
2016-01-25 18:02:04 +0600

    timeline, activity log, todo list colors

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import time as timelib
 from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
+from flask import Response
 # from database import db.session
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import date, datetime, timedelta, time
 from flask.ext.admin import Admin, BaseView, expose #flask-admin
 from flask.ext.admin.contrib.sqla import ModelView
+
 
 # deps so far:
 # dev-python/markdown
@@ -22,6 +25,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://django@localhost/whiplash"
 app.config['SECRET_KEY'] = 'change-that'
 app.config['CSRF_ENABLED'] = True
 app.config['DEBUG'] = True
+
+# import logging
+# from logging import getLogger
+# file_handler = logging.FileHandler(filename="app.log")
+# file_handler.setLevel(logging.WARNING)
+# app.logger.addHandler(file_handler)
+# getLogger('sqlalchemy').addHandler(file_handler)
 
 db = SQLAlchemy(app)
 
@@ -73,6 +83,7 @@ class ListTask(db.Model):
     description = db.Column(db.UnicodeText, default=u"")
     priority = db.Column(db.Integer, default = 50)
     state = db.Column(db.Unicode(10), default = u"ACTIVE")
+    color = db.Column(db.Unicode(8), default = u"")
 
     created_date = db.Column(db.DateTime, default=datetime.now())
     is_time_limited = db.Column(db.Boolean, default = False)
@@ -82,7 +93,7 @@ class ListTask(db.Model):
 
     @property
     def description_markdown(self):
-        return Markup(markdown(self.description))
+        return Markup(markdown(self.description, extensions=['markdown.extensions.tables']))
 
     # def __init__(self, title=None):
     #     self.title = title
@@ -97,6 +108,7 @@ class ListTask(db.Model):
             "description" : self.description_markdown,
             "priority" : self.priority,
             "state" : self.state,
+            "color" : self.color,
             "created_date" : self.created_date.isoformat(),
             "is_time_limited" : self.is_time_limited,
             "expiration_date" : self.expiration_date.isoformat()
@@ -120,6 +132,7 @@ class Task(db.Model):
     pomo_break_length = db.Column(db.Integer, default=5*60)
     pomo_completed = db.Column(db.Integer, default=0)
     pomo_state = db.Column(db.Unicode(12), default=u"INACTIVE")
+    # excuses = relationship("Excuse", back_populates="task")
     # mask = x < daynum
     # repeat & mask
 
@@ -170,6 +183,12 @@ class Task(db.Model):
         progress = int((float(self.task_time)/self.task_length)*100)
         return '<Task %r - %d%%>' % (self.title, progress)
 
+
+# class Excuse(db.Model):
+#     __tablename__ = 'excuses'
+#     id = db.Column(db.Integer, primary_key=True)
+#     task_id = Column(Integer, ForeignKey('tasks.id'))
+#     task = relationship("Task", back_populates="excuses")
 
 
 #flask-admin setup
@@ -239,17 +258,18 @@ def tasks():
     d1 = today - timedelta(days=3)
     d2 = today + timedelta(days=3)
     days_query = Day.query.filter(Day.date >= d1).filter(Day.date < d2).order_by(Day.date).all()
+    ud = UserData.query.all()[0]
     # ud = UserData.query.get(1)
     # if not ud:
     #     ud = UserData()
     #     ud.day_start_date = datetime.now()
     #     db.session.add(ud)
     #     db.session.commit()
-
     return render_template("home.html",
                     tasks=tasks,
                     listtasks=listtasks,
-                    days=dayrange(d1, d2, query=days_query)
+                    days=dayrange(d1, d2, query=days_query),
+                    userdata=ud
                     # day_start_date=us
                     )
 
@@ -292,6 +312,26 @@ def day_start():
             db.session.add(ud)
             db.session.commit()
         return json_response("ok", ud.as_dict(), 200)
+
+@app.route('/api/userdata/water_level', methods=['GET', 'POST'])
+def water_level():
+    if request.method == 'POST':
+        newwater = float(request.form['water_level'])
+        ud = UserData.query.all()[0]
+        ud.water_level = newwater
+        db.session.commit()
+
+        return json_response("ok", ud.as_dict(), 200)
+    else:
+        try:
+            ud = UserData.query.all()[0]
+        except IndexError:
+            ud = UserData()
+            ud.day_start_date = datetime.now()
+            db.session.add(ud)
+            db.session.commit()
+        return json_response("ok", ud.as_dict(), 200)
+
 
 @app.route('/api/listtask/add', methods=['GET', 'POST'])
 def listtask_add():
@@ -341,6 +381,8 @@ def listtask_edit(ltid):
     if request.method == 'POST':
         lt = ListTask.query.get(ltid)
 
+        print(request.form)
+
         if request.form["priority"]:
             new_priority = int(request.form["priority"])
             if new_priority > 100:
@@ -354,6 +396,8 @@ def listtask_edit(ltid):
 
         if request.form["title"]:
             lt.title = request.form["title"]
+
+        lt.color = request.form["color"] or ""
         
         db.session.commit()
 
@@ -401,8 +445,89 @@ def taskjson(task_id):
 # def shutdown_session(exception=None):
 #     db.session.remove()
 
+import pika
+
+def event_stream():
+    # pubsub = red.pubsub()
+    # pubsub.subscribe('chat')
+    # for message in pubsub.listen():
+        # print message
+    channel.queue_declare(queue='Q1')
+
+    
+
+    for method, properties, body in channel.consume('Q1'):
+        # print body
+        channel.basic_ack(method.delivery_tag)
+
+        # method, properties, body
+        # r = channel.consume(queue='Q1')
+        # body="asdf"
+        # channel.basic_ack()
+
+        print("received message:", body)
+        yield 'data: %s\n\n' % body
+
+@app.route('/stream')
+def stream():
+    print("stream entered")
+    return Response(event_stream(),
+                          mimetype="text/event-stream")
+
+
+MBOX = {}
+LastPollTable = {}
+
+@app.route('/post', methods=['POST'])
+def post():
+    # for sid,queue in MBOX.items():
+        # queue.append("event5123")
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+    channel = connection.channel()
+    channel.exchange_declare(exchange='events', type='fanout')
+
+    print(request.form['event'])
+    data = request.form['data']
+    if request.form['event']:
+        message = "{0}:{1}".format(request.form['event'], data)
+    else:
+        message = "{0}".format(data)
+
+    channel.basic_publish(exchange='events',
+                      routing_key='',
+                      body=message)
+
+    connection.close()
+
+    return "OK", 200
+
+
+@app.route('/api/events', methods=['GET', 'POST'])
+def events():
+    sessionID = request.form["sessionID"]
+    if not sessionID in MBOX:
+        MBOX[sessionID] = []
+        now = timelib.time()
+        LastPollTable[sessionID] = now
+        # print(MBOX)
+
+        #cleanup here too
+        for sid in MBOX.keys():
+            if now > LastPollTable[sid] + 180:
+                del MBOX[sid]
+                del LastPollTable[sid]
+
+    while len(MBOX[sessionID]) > 0:
+        event = MBOX[sessionID].pop(0)
+        return "{ status: 'OK', event: %s }" % event, 200
+
+    return "{ status: 'OK' }", 200
+
+
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(host='nevihta.d87')
+    app.run(host='0.0.0.0')
+    # app.run(host='nevihta.d87')
